dc53079fe24a53180de97e2edcfcd5929a69662d
Bassanteltaweel99 <passantzakaria0@gmail.com>
2023-07-15 04:46:39 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
 * main - prints the alphabet.
 *
 *Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;

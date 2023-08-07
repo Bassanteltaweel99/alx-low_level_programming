a3e32f184d36375809235ed0bb63dcd83e332b55
Bassanteltaweel99 <passantzakaria0@gmail.com>
2023-08-07 10:19:07 -0700

    Finish

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -21,7 +21,6 @@ char **strtow(char *str)
 	if (*str == '\0' || str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
 			w++;
 	}
@@ -51,14 +50,10 @@ char **strtow(char *str)
 				return (NULL);
 }
 for (m = 0; wordf < i; wordf++)
-{
 p[j][m] = str[wordf];
 m++;
-}
 p[j][m] = '\0';
 j++;
-}
-}
 p[j] = NULL;
 return (p);
 }
